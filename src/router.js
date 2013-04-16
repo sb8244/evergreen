8de76b9b54bc3a52ded1255bd5bcb52984b8bc09
Stephen Bussey <sb8244@ubuntu.(none)>
2013-04-16 04:41:43 -0700

    Reverting test readme edit

--- src/router.js
+++ src/router.js
@@ -9,5 +9,11 @@ exports.create = function( app ) {
 	 * Index Routes
 	 */
 	//app.get('/', index.index);
-	app.get('/', index.index);
+	app.get('/', function() {
+		var UserProvider = require("./models/user").UserProvider;
+		var users = new UserProvider();
+		users.findById("516cef0cddede7b843180de0", function(err, res) {
+			console.log(res);
+		})
+	});
 }
